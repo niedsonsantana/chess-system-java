cd5c84611904995aa086950fa4ade02be384223a
Niedson Santana <niedsonsantana@hotmail.com>
2021-03-04 22:43:03 -0300

    Tratamento de exceções e limpesa de tela após cada jogada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,40 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//		Board board = new Board(8,8);
 		ChessMatch chessMatch = new ChessMatch();
+
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("source");
-			ChessPosition source = UI.readChessPosition(sc);
-			System.out.println();
-			System.out.println("Target");
-			ChessPosition target = UI.readChessPosition(sc);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.println("source");
+				ChessPosition source = UI.readChessPosition(sc);
+				System.out.println();
+				System.out.println("Target");
+				ChessPosition target = UI.readChessPosition(sc);
 
-			ChessPiece capturePiece = chessMatch.performChessMove(source, target);
-
-//		Position pos = new Position(3,5);
-//		System.out.println(pos);
-//		King k = new King(board, Color.BLACK);
-			//System.out.print(k);
+				ChessPiece capturePiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
