d07f9e71155b5e54aee73e9112406aab5be287a6
Niedson Santana <niedsonsantana@hotmail.com>
2021-03-09 15:29:50 -0300

    Atualização na promoção de peões

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,9 +4,6 @@ import boardGame.Board;
 import boardGame.Piece;
 import boardGame.Position;
 import chess.pieces.*;
-
-import javax.xml.transform.Source;
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -113,7 +110,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if(!type.equals("B") && !type.equals("N") && type.equals("R") && !type.equals("Q")){
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
